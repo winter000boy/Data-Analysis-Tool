2a8e5cf1a5267e93387f7d4eb33a0771001edaa4
Durgesh Sharma <155382388+winter000boy@users.noreply.github.com>
2025-01-22 22:50:43 +0530

    yo

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ st.sidebar.title("Upload Dataset")
 Upload_File = st.sidebar.file_uploader("Choose csv file", type='csv')
 if Upload_File is not None:
     df = pd.read_csv(Upload_File)
-
+    no_of_event = len(df)
     citizenship_count = df['citizenship'].value_counts
     event_location_region = df['event_location_region'].value_counts
     hostilities_count = df[df['took_part_in_the_hostilities'] == 'yes']['citizenship'].value_counts()
     noHostilitiesCount = df[df['took_part_in_the_hostilities'] == 'No']['citizenship'].value_counts()
+
+
+    st.sidebar.write("No of Events : ",no_of_event)
+    
