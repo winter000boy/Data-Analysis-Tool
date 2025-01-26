7f0772cfbffffe496836db0ad978bfffcc980667
Durgesh Sharma <155382388+winter000boy@users.noreply.github.com>
2025-01-26 13:27:44 +0530

    Second Last Commit

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ if Upload_File is not None:
 
     col1,col2,col3,col4 = st.sidebar.columns(4)
     with col1:
-        st.sidebar.subheader("Citizenship Count")
-        st.sidebar.write(citizenship_count())
+        st.subheader("Citizenship Count")
+        st.write(citizenship_count())
 
     with col2:
-        st.sidebar.subheader("Event Location Region")
-        st.sidebar.write(event_location_region())
+        st.subheader("Event Location Region")
+        st.write(event_location_region())
 
     with col3:
         st.sidebar.subheader("Hostilities Count")
@@ -34,6 +34,50 @@ if Upload_File is not None:
     with col4:
         st.sidebar.subheader("Non Hostilities Count")
         st.sidebar.write(noHostilitiesCount)
+
+    wepons_counts = df['ammunition'].value_counts
+    st.sidebar.subheader("Wepon Counts")
+    st.sidebar.write(wepons_counts())
+
+
+
+    ## Data Analysis Part
+    st.title("Israel Palestine Data Analysis")
+    st.write("Data Sample",df)
+
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Type of Injuries")
+        typeOfInjury = df['type_of_injury'].value_counts()
+        st.bar_chart(typeOfInjury)
+    
+    with col2:
+        st.subheader("Distribution By Gender")
+        gender = df['gender'].value_counts()
+        st.bar_chart(gender, color='#FF0000')
+
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("Age Distribution of war")
+        age = df['age'].describe()
+        st.write(age)
+
+    with col2:
+        event_location_region = df['event_location_region'].value_counts()
+        st.subheader("Event Locations")
+        st.bar_chart(event_location_region)
+    
+
+    
+
+
+    
+
+
+    
 
 
 
